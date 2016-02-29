ea9b4a981f98f170f619d4d376691bdc63408ea3
Arjan de Ruijter <rra@mboutrecht.nl>
2016-02-29 15:38:51 +0100

    Dit is nu werkend

--- app/src/main/java/com/adruijter/koffiebestelappje/MainActivity.java
+++ app/src/main/java/com/adruijter/koffiebestelappje/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.CalendarView;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,6 +15,18 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
     //Fields
     CalendarView cld;
+    EditText txtFirstname,
+             txtInfix,
+             txtLastname,
+             txtNumberOfCoffee,
+             txtDebugText;
+    CheckBox chkbxMilk,
+             chkbxSugar;
+    SeekBar  skbar;
+    Date     date;
+    String   test = "";
+
+
 
 
     @Override
@@ -21,10 +34,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.cld = (CalendarView) findViewById(R.id.calendar);
+        this.txtFirstname  = (EditText) findViewById(R.id.firstname);
+        this.txtInfix =  (EditText) findViewById(R.id.infix);
+        this.txtLastname = (EditText) findViewById(R.id.lastname);
+        this.txtNumberOfCoffee = (EditText) findViewById(R.id.numberOfCoffee);
+        this.txtDebugText = (EditText) findViewById(R.id.debugTekst);
+        this.chkbxMilk = (CheckBox) findViewById(R.id.milk);
+        this.chkbxSugar = (CheckBox) findViewById(R.id.sugar);
+        this.skbar = (SeekBar) findViewById(R.id.seek_bar);
         this.cld.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
 
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                test = dayOfMonth + "/" + (month + 1) + "/" + year;
+                Toast.makeText(getApplicationContext(), test, Toast.LENGTH_SHORT).show();
+                bestelKoffie(view);
             }
         });
     }
@@ -54,39 +78,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String readableDate(CalendarView cld){
-        Date date = new Date(cld.getDate());
+        this.date = new Date(cld.getDate());
         String dateOutput = new SimpleDateFormat("dd-MM-yyyy").format(date);
         return dateOutput;
     }
 
     public void bestelKoffie(View view)
     {
-        EditText txtFirstname = (EditText) findViewById(R.id.firstname);
-        EditText txtInfix =  (EditText) findViewById(R.id.infix);
-        EditText txtLastname = (EditText) findViewById(R.id.lastname);
-        EditText txtNumberOfCoffee = (EditText) findViewById(R.id.numberOfCoffee);
-        EditText txtDebugText = (EditText) findViewById(R.id.debugTekst);
-        CheckBox chkbxMilk = (CheckBox) findViewById(R.id.milk);
-        CheckBox chkbxSugar = (CheckBox) findViewById(R.id.sugar);
-        SeekBar skbar = (SeekBar) findViewById(R.id.seek_bar);
-
-
+        this.cld = (CalendarView) findViewById(R.id.calendar);
         String output = String.format("Voornaam: %s \n" +
-                                      "Tussenvoegsel: %s \n" +
-                                      "Achternaam: %s \n" +
-                                      "Aantal kopjes koffie: %s \n" +
-                                      "Wel of geen melk: %s \n" +
-                                      "Wel of geen suiker: %s \n" +
-                                      "Koffie sterkte %s \n" +
-                                      "Datum: %s",
-                                      txtFirstname.getText(),
-                                      txtInfix.getText(),
-                                      txtLastname.getText(),
-                                      txtNumberOfCoffee.getText(),
-                                      this.milkYesOrNo(chkbxMilk),
-                                      this.sugarYesOrNo(chkbxSugar),
-                                      skbar.getProgress(),
-                                      this.readableDate(this.cld));
+                        "Tussenvoegsel: %s \n" +
+                        "Achternaam: %s \n" +
+                        "Aantal kopjes koffie: %s \n" +
+                        "Wel of geen melk: %s \n" +
+                        "Wel of geen suiker: %s \n" +
+                        "Koffie sterkte %s \n" +
+                        "Datum: %s",
+                this.txtFirstname.getText(),
+                this.txtInfix.getText(),
+                this.txtLastname.getText(),
+                this.txtNumberOfCoffee.getText(),
+                this.milkYesOrNo(chkbxMilk),
+                this.sugarYesOrNo(chkbxSugar),
+                this.skbar.getProgress(),
+                //this.readableDate(this.cld));
+                this.test);
         txtDebugText.setText(output);
+        Toast.makeText(getApplicationContext(), "Hallo dan!", Toast.LENGTH_SHORT).show();
     }
 }
