4adc0327b7df0d17a7eb55819f448b69bd1c7750
Arjan de Ruijter <rra@mboutrecht.nl>
2016-03-11 13:05:29 +0100

    TextView aangemaakt

--- app/src/main/java/com/adruijter/koffiebestelappje/LoadActivity.java
+++ app/src/main/java/com/adruijter/koffiebestelappje/LoadActivity.java
@@ -4,13 +4,22 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class LoadActivity extends Activity {
+    //Fields
+    TextView dataTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load);
+        this.dataTxt = (TextView)findViewById(R.id.internMemText);
     }
 
 
@@ -18,5 +27,35 @@ public class LoadActivity extends Activity {
     {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void showOrders(View view)
+    {
+
+        Toast.makeText(getBaseContext(),"Hallo de knop werkt", Toast.LENGTH_LONG).show();
+
+        FileInputStream fis;
+        int read;
+        StringBuffer buffer = new StringBuffer();
+        try {
+            fis = openFileInput("bestelling.txt");
+
+            while((read = fis.read()) != -1)
+            {
+                buffer.append((char)read);
+            }
+
+            String output = buffer.substring(0, buffer.length());
+
+            Toast.makeText(getBaseContext(), output, Toast.LENGTH_LONG).show();
+        }
+        catch(FileNotFoundException e)
+        {
+            e.getMessage();
+        }
+        catch(IOException e)
+        {
+            e.getMessage();
+        }
     }
 }
