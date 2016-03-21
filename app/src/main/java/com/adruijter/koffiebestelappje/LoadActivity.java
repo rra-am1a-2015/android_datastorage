095d9b104beb61e5d9660cefcdf3a443f0ca2f65
Arjan de Ruijter <rra@mboutrecht.nl>
2016-03-21 15:38:25 +0100

    Table

--- app/src/main/java/com/adruijter/koffiebestelappje/LoadActivity.java
+++ app/src/main/java/com/adruijter/koffiebestelappje/LoadActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +21,8 @@ import java.util.List;
 public class LoadActivity extends Activity {
     //Fields
     TextView dataTxt;
+    private static final int NUM_ROWS = 4;
+    private static final int NUM_COLS = 9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +41,68 @@ public class LoadActivity extends Activity {
     public void showOrders(View view)
     {
 
-        //Toast.makeText(getBaseContext(),"Hallo de knop werkt", Toast.LENGTH_LONG).show();
+        TableLayout table = (TableLayout) findViewById(R.id.tableInternalMemory);
+
+        for ( int row = 0; row < NUM_ROWS; row++)
+        {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    TableLayout.LayoutParams.MATCH_PARENT,
+                    1.0f
+            ));
+            table.addView(tableRow);
+
+            for ( int col = 0; col < NUM_COLS; col++)
+            {
+                Button button = new Button(this);
+                button.setText("" + row + ", " + col);
+                button.setPadding(0, 0 , 0, 0);
+                button.setTextSize(9);
+                button.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        1.0f
+                ));
+                tableRow.addView(button);
+            }
+        }
+
+
 
         FileInputStream fis;
         int read;
-        StringBuffer buffer = new StringBuffer();
-        ArrayList<String> test = new ArrayList<String>();
+
+
+
+
         int index = 0;
         int teller = 0;
+
         try {
+
+            String[] sentences;
             fis = openFileInput("bestelling.txt");
+            StringBuffer buffer = new StringBuffer();
 
             while((read = fis.read()) != -1)
             {
-                buffer.append((char) read);
+                buffer.append((char)read);
+            }
+            String output = buffer.substring(0, buffer.length());
 
-                /*
+            sentences = output.split(System.getProperty("line.separator"));
+
+            Log.v("test123", sentences[1] );
+
+            this.dataTxt.setText(output);
+
+
+
+            //Toast.makeText(getBaseContext(),"Hallo de knop werkt", Toast.LENGTH_LONG).show();
+            //Toast.makeText(getBaseContext(), output, Toast.LENGTH_LONG).show();
+            // test.addAll(Arrays.asList(output.split(" ")));
+            /*
                 if (buffer.lastIndexOf(" ") != -1)
                 {
                     test.add(buffer.substring(index, buffer.length()));
@@ -62,22 +113,7 @@ public class LoadActivity extends Activity {
                     teller++;
                 }
                 */
-
-
-                //Toast.makeText(getApplicationContext(), buffer.substring(0, buffer.length()) +  Integer.toString(buffer.lastIndexOf(" ")) + test.get(0), Toast.LENGTH_SHORT).show();
-            }
-
-            String output = buffer.substring(0, buffer.length());
-           // test.addAll(Arrays.asList(output.split(" ")));
-
-
-
-
-            //Toast.makeText(getBaseContext(), output, Toast.LENGTH_LONG).show();
-            Log.v("test", );
-
-            this.dataTxt.setText(output);
-
+            //Toast.makeText(getApplicationContext(), buffer.substring(0, buffer.length()) +  Integer.toString(buffer.lastIndexOf(" ")) + test.get(0), Toast.LENGTH_SHORT).show();
         }
         catch(FileNotFoundException e)
         {
